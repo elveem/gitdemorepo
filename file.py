3a19093c80caff62e9cbe3c27969ea4244a8d907
elveem <elveemalakar@gmail.com>
2024-07-23 14:56:17 +0530

    file

---
+++ file.py
@@ -0,0 +1,8 @@
+# This is the sample python script for Git Demo
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = read.csv("sample_data.csv")
+df.head(5)
+
+plt.plot(df['col1'], df['col2])
